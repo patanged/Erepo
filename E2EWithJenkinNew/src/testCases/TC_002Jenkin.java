84944046b80363524817eaacfcb2b801d281963c
DELL <DELL@DESKTOP-9VERA0T>
2022-11-12 13:30:58 +0530

    This is new File for Jenkin Program

---
+++ E2EWithJenkinNew/src/testCases/TC_002Jenkin.java
@@ -0,0 +1,22 @@
+package testCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_002Jenkin {
+
+	WebDriver Driver;
+	@Test
+	public void testcase()
+	{
+		System.setProperty("webdriver.chrome.driver", "./DriverFile/chromedriver_win32/chromedriver.exe");
+		Driver = new ChromeDriver();
+		Driver.manage().window().maximize();
+		Driver.get("https://www.facebook.com/");
+		Driver.findElement(By.id("email")).sendKeys("Dinesh patange");
+		Driver.quit();		
+	}
+  
+}
